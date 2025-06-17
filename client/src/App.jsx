c2760e4101272166be1093ccfdcdc6c07c10bba1
Limon00001 <limonhossain82@gmail.com>
2025-06-17 12:30:38 +0600

    feat(client-app): implement routing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,43 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// External Imports
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+// Internal Imports
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Expense from './pages/Dashboard/Expense';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/sign-up" exact element={<SignUp />} />
+        <Route path="/dashboard" exact element={<Home />} />
+        <Route path="/income" exact element={<Income />} />
+        <Route path="/expense" exact element={<Expense />} />
+      </Routes>
+    </div>
+  );
 };
 
 // Export
 export default App;
+
+// Root Component
+const Root = () => {
+  // Check if user is authenticated
+  const isAuthenticated = localStorage.getItem('token');
+
+  // If authenticated, redirect to dashboard
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
